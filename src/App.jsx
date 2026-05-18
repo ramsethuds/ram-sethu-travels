d7765b1ac9fab946e307c0526bc5e275b8c871ba
ramsethuds <ramsethuds@gmail.com>
2026-05-18 18:27:00 +0530

    added google map

--- src/App.jsx
+++ src/App.jsx
@@ -170,7 +170,17 @@ export default function App() {
       <section className="contact">
 
         <h2>Contact Us</h2>
-
+<div style={{ marginTop: "40px" }}>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d243.0287666048835!2d77.65377940947299!3d12.942393946737157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1779107826500!5m2!1sen!2sin"
+    width="100%"
+    height="450"
+    style={{ border: 0, borderRadius: "20px" }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</div>
         <div className="contact-grid">
 
           <div className="contact-box">
